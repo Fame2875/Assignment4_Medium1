4ee40f02c4f1cbc8605c8f56a106a765a4cb6efd
FameNew <63010185@KMITL.AC.TH>
2022-06-20 16:34:50 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment4_Medium1';
+  userInput: number = 0;
+  toggle = false;
+  Primelist: Array<any> = [];
+  Primenum() {
+    this.toggle = !this.toggle;
+    for (var i = 0; i <= this.userInput; i++) {
+      var flag = 0;
+      for (var j = 2; j < i; j++) {
+        if (i % j == 0) {
+          flag = 1;
+          break;
+        }
+      }
+      if (i > 1 && flag == 0) {
+        console.log(i);
+        this.Primelist.push(i);
+      }
+    }
+    
+  }
 }
