b32cc4bf29101466b0cba7fdeda50f54ed1a6829
FameNew <63010185@KMITL.AC.TH>
2022-06-23 10:44:18 +0700

    secound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,33 @@ export class AppComponent {
   title = 'Assignment4_Medium1';
   userInput: number = 0;
   toggle = false;
+  clear = true;
   Primelist: Array<any> = [];
   Primenum() {
     this.toggle = !this.toggle;
-    for (var i = 0; i <= this.userInput; i++) {
-      var flag = 0;
-      for (var j = 2; j < i; j++) {
-        if (i % j == 0) {
-          flag = 1;
-          break;
+    if(this.toggle == false){
+    }
+    else{
+      if (this.clear == false){
+        console.log(this.clear);
+        this.Primelist = [''];
+        this.clear = true;
+      }
+        for (var i = 0; i <= this.userInput; i++) {
+        var flag = 0;
+        for (var j = 2; j < i; j++) {
+          if (i % j == 0) {
+            flag = 1;
+            break;
+          }
+        }
+        if (i > 1 && flag == 0) {
+          console.log(i);
+          this.Primelist.push(i);
         }
       }
-      if (i > 1 && flag == 0) {
-        console.log(i);
-        this.Primelist.push(i);
-      }
+      this.clear = false;
     }
-    
-  }
+    }
+
 }
